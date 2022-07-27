e6eb2f62d3c4bfd63592c1e1dc03a48c7f153643
RaisaHubareva <ivan.af@mail.ru>
2022-07-27 21:45:20 +0300

    feat: modifide index js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n)
+      n = String(n);
+
+      let revNum = n[0];
+      for (let i = 1; i < n.length; i++) {
+          revNum = `${n[i]}${revNum}`;
+
+      }
+
+      return revNum;   
 }
